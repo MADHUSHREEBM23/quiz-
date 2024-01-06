ea5d7e3ef24e7507455624ae338b594c92799fb5
Madhushree B M <153265039+MADHUSHREEBM23@users.noreply.github.com>
2024-01-06 11:25:37 +0530

    Add files via upload

---
+++ qu.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question:'What is the capital of France?',
+        options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+        correctAnswer: 'Paris'
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+        correctAnswer: 'Mars'
+    },
+    {
+        question: 'What is the largest mammal?',
+        options: ['Elephant', 'Whale', 'Giraffe', 'Hippopotamus'],
+        correctAnswer: 'Whale'
+    }
+];
